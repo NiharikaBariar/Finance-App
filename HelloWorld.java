434e57e66a4001fe0e89398a46140c9c0ca4d16c
Niharika Bariar <niharikaa2050@gmail.com>
2021-04-20 20:02:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 // This program will print messages on the command line
-public class Assignment_1 {
+public class Assignment1 {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
